8c97a86e204626f1c0719a49cfced77768aa0ec3
marialokodi <mrialokodi@gmail.com>
2022-03-23 18:05:16 +0200

    Homework for 23.03.2022.

--- curs 6/HW/Javascript/challenge.js
+++ curs 6/HW/Javascript/challenge.js
@@ -7,36 +7,32 @@
 
 1 0 1 0*/
 
-function pattern() {
-  for (let i = 0; i < 4; i++) {
-    for (let i = 0; i < 4; i++) {
-      if (i % 2 === 0) {
-        console.log(0);
-      } else {
-        console.log(1);
-      }
+function pattern1() {
+  let str = "";
+  let p = 0;
+  for (let i = 0; i < 16; i++) {
+    str += p;
+    if (i % 4 === 3) {
+      str += "\n";
+      p = (p + 1) % 2;
     }
-    /*    for (let i = 0; i < 4; i++) {
-      if (i % 2 === 0) {
-        return 1;
-      } else {
-        return 0;
-      }
-    }
-    for (let i = 0; i < 4; i++) {
-      if (i % 2 === 0) {
-        return 0;
-      } else {
-        return 1;
-      }
-    }
-    for (let i = 0; i < 4; i++) {
-      if (i % 2 === 0) {
-        return 1;
-      } else {
-        return 0;
-      }
-    }*/
+    p = (p + 1) % 2;
   }
+  return str;
 }
-console.log(pattern());
+
+function pattern2() {
+  let str = "";
+  let p = 0;
+  for (let i = 0; i < 16; i++) {
+    str += p;
+    if (i % 4 === 3) {
+      str += "\n";
+    } else {
+      p = (p + 1) % 2;
+    }
+  }
+  return str;
+}
+console.log(pattern1());
+console.log(pattern2());
